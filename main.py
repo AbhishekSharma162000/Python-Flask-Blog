7c16d2df115670e72f542097ba9a844ee108a921
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2022-02-03 23:25:40 +0530

    Fixing the add post bug & add Flashing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from mysql import connector
@@ -52,6 +52,8 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
+    # flash("Welcome To My Channel", "success")
+    # flash("COVID-19 alert - STAY HOME, STAY SAFE", "danger")
     posts = Posts.query.filter_by().all()
     last = math.ceil(len(posts)/int(params['no_of_posts']))
     # [0:params['no_of_posts']]
@@ -172,6 +174,7 @@ def contact():
                           recipients=[params['gmail-user']],
                           body=message + "\n" + phone
                           )
+        flash("Thanks for submitting your details. We will get back back to you soon.", "success")
     return render_template('contact.html', params=params)
 
 
